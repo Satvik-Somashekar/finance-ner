541d550f37d8efccbd3ba7d5d5cdf47f8b983536
Satvik S <167232125+Satvik-Somashekar@users.noreply.github.com>
2026-04-13 12:27:11 +0530

    Port tagger.py to Python 3 with argparse
    Updated tagger.py to use argparse instead of optparse for argument parsing and modified print statements for Python 3 compatibility.

--- tagger.py
+++ tagger.py
@@ -1,37 +1,24 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+# Modified by Satvik Somashekar - Python 3 port + finance NER support
 
 import os
 import time
 import codecs
-import optparse
+import argparse          # CHANGED: optparse is deprecated in Python 3
 import json
 import numpy as np
 from loader import prepare_sentence
 from utils import create_input, iobes_iob, iob_ranges, zero_digits
 from model import Model
 
-optparser = optparse.OptionParser()
-optparser.add_option(
-    "-m", "--model", default="",
-    help="Model location"
-)
-optparser.add_option(
-    "-i", "--input", default="",
-    help="Input file location"
-)
-optparser.add_option(
-    "-o", "--output", default="",
-    help="Output file location"
-)
-optparser.add_option(
-    "-d", "--delimiter", default="__",
-    help="Delimiter to separate words from their tags"
-)
-optparser.add_option(
-    "--outputFormat", default="",
-    help="Output file format"
-)
-opts = optparser.parse_args()[0]
+# CHANGED: argparse instead of optparse
+parser = argparse.ArgumentParser(description="NER Tagger - Finance Domain")
+parser.add_argument("-m", "--model", default="", help="Model location")
+parser.add_argument("-i", "--input", default="", help="Input file location")
+parser.add_argument("-o", "--output", default="", help="Output file location")
+parser.add_argument("-d", "--delimiter", default="__", help="Delimiter to separate words from their tags")
+parser.add_argument("--outputFormat", default="", help="Output file format (json or default)")
+opts = parser.parse_args()
 
 # Check parameters validity
 assert opts.delimiter
@@ -39,7 +26,7 @@ assert os.path.isdir(opts.model)
 assert os.path.isfile(opts.input)
 
 # Load existing model
-print "Loading model..."
+print("Loading model...")          # CHANGED: print() with parentheses
 model = Model(model_path=opts.model)
 parameters = model.parameters
 
@@ -56,45 +43,47 @@ model.reload()
 f_output = codecs.open(opts.output, 'w', 'utf-8')
 start = time.time()
 
-print 'Tagging...'
+print('Tagging...')                # CHANGED: print() with parentheses
 with codecs.open(opts.input, 'r', 'utf-8') as f_input:
     count = 0
     for line in f_input:
         words_ini = line.rstrip().split()
         if line:
-            # Lowercase sentence
             if parameters['lower']:
                 line = line.lower()
-            # Replace all digits with zeros
             if parameters['zeros']:
                 line = zero_digits(line)
             words = line.rstrip().split()
-            # Prepare input
+
             sentence = prepare_sentence(words, word_to_id, char_to_id,
                                         lower=parameters['lower'])
             input = create_input(sentence, parameters, False)
-            # Decoding
+
             if parameters['crf']:
                 y_preds = np.array(f_eval(*input))[1:-1]
             else:
                 y_preds = f_eval(*input).argmax(axis=1)
             y_preds = [model.id_to_tag[y_pred] for y_pred in y_preds]
-            # Output tags in the IOB2 format
+
             if parameters['tag_scheme'] == 'iobes':
                 y_preds = iobes_iob(y_preds)
-            # Write tags
+
             assert len(y_preds) == len(words)
-            
             if opts.outputFormat == 'json':
-                f_output.write(json.dumps({ "text": ' '.join(words), "ranges": iob_ranges(y_preds) }))
+                f_output.write(json.dumps({
+                    "text": ' '.join(words),
+                    "ranges": iob_ranges(y_preds)
+                }))
             else:
-                f_output.write('%s\n' % ' '.join('%s%s%s' % (w, opts.delimiter, y)
-                                                 for w, y in zip(words_ini, y_preds)))
+                f_output.write('%s\n' % ' '.join(
+                    '%s%s%s' % (w, opts.delimiter, y)
+                    for w, y in zip(words_ini, y_preds)
+                ))
         else:
             f_output.write('\n')
         count += 1
         if count % 100 == 0:
-            print count
+            print(count)           # CHANGED: print() with parentheses
 
-print '---- %i lines tagged in %.4fs ----' % (count, time.time() - start)
+print('---- %i lines tagged in %.4fs ----' % (count, time.time() - start))  # CHANGED
 f_output.close()
