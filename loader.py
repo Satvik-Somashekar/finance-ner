c8a981fefc4ff959d8aa4ba17a6908f3c0f0df57
Satvik S <167232125+Satvik-Somashekar@users.noreply.github.com>
2026-04-13 12:30:02 +0530

    Port loader.py to Python 3 and update print statements

--- loader.py
+++ loader.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# Modified by Satvik Somashekar - Python 3 port
+
 import os
 import re
 import codecs
@@ -6,10 +9,6 @@ from utils import iob2, iob_iobes
 
 
 def load_sentences(path, lower, zeros):
-    """
-    Load sentences. A line must contain at least a word and its tag.
-    Sentences are separated by empty lines.
-    """
     sentences = []
     sentence = []
     for line in codecs.open(path, 'r', 'utf8'):
@@ -30,19 +29,13 @@ def load_sentences(path, lower, zeros):
 
 
 def update_tag_scheme(sentences, tag_scheme):
-    """
-    Check and update sentences tagging scheme to IOB2.
-    Only IOB1 and IOB2 schemes are accepted.
-    """
     for i, s in enumerate(sentences):
         tags = [w[-1] for w in s]
-        # Check that tags are given in the IOB format
         if not iob2(tags):
             s_str = '\n'.join(' '.join(w) for w in s)
-            raise Exception('Sentences should be given in IOB format! ' +
+            raise Exception('Sentences should be given in IOB format! '
                             'Please check sentence %i:\n%s' % (i, s_str))
         if tag_scheme == 'iob':
-            # If format was IOB1, we convert to IOB2
             for word, new_tag in zip(s, tags):
                 word[-1] = new_tag
         elif tag_scheme == 'iobes':
@@ -54,49 +47,32 @@ def update_tag_scheme(sentences, tag_scheme):
 
 
 def word_mapping(sentences, lower):
-    """
-    Create a dictionary and a mapping of words, sorted by frequency.
-    """
     words = [[x[0].lower() if lower else x[0] for x in s] for s in sentences]
     dico = create_dico(words)
     dico['<UNK>'] = 10000000
     word_to_id, id_to_word = create_mapping(dico)
-    print "Found %i unique words (%i in total)" % (
-        len(dico), sum(len(x) for x in words)
-    )
+    print("Found %i unique words (%i in total)" % (         # CHANGED: print()
+        len(dico), sum(len(x) for x in words)))
     return dico, word_to_id, id_to_word
 
 
 def char_mapping(sentences):
-    """
-    Create a dictionary and mapping of characters, sorted by frequency.
-    """
     chars = ["".join([w[0] for w in s]) for s in sentences]
     dico = create_dico(chars)
     char_to_id, id_to_char = create_mapping(dico)
-    print "Found %i unique characters" % len(dico)
+    print("Found %i unique characters" % len(dico))         # CHANGED: print()
     return dico, char_to_id, id_to_char
 
 
 def tag_mapping(sentences):
-    """
-    Create a dictionary and a mapping of tags, sorted by frequency.
-    """
     tags = [[word[-1] for word in s] for s in sentences]
     dico = create_dico(tags)
     tag_to_id, id_to_tag = create_mapping(dico)
-    print "Found %i unique named entity tags" % len(dico)
+    print("Found %i unique named entity tags" % len(dico))  # CHANGED: print()
     return dico, tag_to_id, id_to_tag
 
 
 def cap_feature(s):
-    """
-    Capitalization feature:
-    0 = low caps
-    1 = all caps
-    2 = first letter caps
-    3 = one capital (not first letter)
-    """
     if s.lower() == s:
         return 0
     elif s.upper() == s:
@@ -108,37 +84,22 @@ def cap_feature(s):
 
 
 def prepare_sentence(str_words, word_to_id, char_to_id, lower=False):
-    """
-    Prepare a sentence for evaluation.
-    """
     def f(x): return x.lower() if lower else x
     words = [word_to_id[f(w) if f(w) in word_to_id else '<UNK>']
              for w in str_words]
     chars = [[char_to_id[c] for c in w if c in char_to_id]
              for w in str_words]
     caps = [cap_feature(w) for w in str_words]
-    return {
-        'str_words': str_words,
-        'words': words,
-        'chars': chars,
-        'caps': caps
-    }
+    return {'str_words': str_words, 'words': words, 'chars': chars, 'caps': caps}
 
 
 def prepare_dataset(sentences, word_to_id, char_to_id, tag_to_id, lower=False):
-    """
-    Prepare the dataset. Return a list of lists of dictionaries containing:
-        - word indexes
-        - word char indexes
-        - tag indexes
-    """
     def f(x): return x.lower() if lower else x
     data = []
     for s in sentences:
         str_words = [w[0] for w in s]
         words = [word_to_id[f(w) if f(w) in word_to_id else '<UNK>']
                  for w in str_words]
-        # Skip characters that are not in the training set
         chars = [[char_to_id[c] for c in w if c in char_to_id]
                  for w in str_words]
         caps = [cap_feature(w) for w in str_words]
@@ -154,25 +115,13 @@ def prepare_dataset(sentences, word_to_id, char_to_id, tag_to_id, lower=False):
 
 
 def augment_with_pretrained(dictionary, ext_emb_path, words):
-    """
-    Augment the dictionary with words that have a pretrained embedding.
-    If `words` is None, we add every word that has a pretrained embedding
-    to the dictionary, otherwise, we only add the words that are given by
-    `words` (typically the words in the development and test sets.)
-    """
-    print 'Loading pretrained embeddings from %s...' % ext_emb_path
+    print('Loading pretrained embeddings from %s...' % ext_emb_path)  # CHANGED
     assert os.path.isfile(ext_emb_path)
-
-    # Load pretrained embeddings from file
     pretrained = set([
         line.rstrip().split()[0].strip()
         for line in codecs.open(ext_emb_path, 'r', 'utf-8')
         if len(ext_emb_path) > 0
     ])
-
-    # We either add every word in the pretrained file,
-    # or only words given in the `words` list to which
-    # we can assign a pretrained embedding
     if words is None:
         for word in pretrained:
             if word not in dictionary:
@@ -182,9 +131,8 @@ def augment_with_pretrained(dictionary, ext_emb_path, words):
             if any(x in pretrained for x in [
                 word,
                 word.lower(),
-                re.sub('\d', '0', word.lower())
+                re.sub(r'\d', '0', word.lower())   # CHANGED: raw string for regex
             ]) and word not in dictionary:
                 dictionary[word] = 0
-
     word_to_id, id_to_word = create_mapping(dictionary)
     return dictionary, word_to_id, id_to_word
