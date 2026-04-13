f432aaffe0b62756f89c7c5416e76d82962fc2ff
Satvik S <167232125+Satvik-Somashekar@users.noreply.github.com>
2026-04-13 12:35:53 +0530

    Port model.py to Python 3 compatibility
    Updated model.py to be compatible with Python 3 by replacing cPickle with pickle, modifying print statements, and changing xrange to range.

--- model.py
+++ model.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# Modified by Satvik Somashekar - Python 3 port
+
 import os
 import re
 import numpy as np
@@ -5,7 +8,7 @@ import scipy.io
 import theano
 import theano.tensor as T
 import codecs
-import cPickle
+import pickle                               # CHANGED: cPickle → pickle (removed in Python 3)
 
 from utils import shared, set_values, get_name
 from nn import HiddenLayer, EmbeddingLayer, DropoutLayer, LSTM, forward
@@ -17,42 +20,29 @@ class Model(object):
     Network architecture.
     """
     def __init__(self, parameters=None, models_path=None, model_path=None):
-        """
-        Initialize the model. We either provide the parameters and a path where
-        we store the models, or the location of a trained model.
-        """
         if model_path is None:
             assert parameters and models_path
-            # Create a name based on the parameters
             self.parameters = parameters
             self.name = get_name(parameters)
-            # Model location
             model_path = os.path.join(models_path, self.name)
             self.model_path = model_path
             self.parameters_path = os.path.join(model_path, 'parameters.pkl')
             self.mappings_path = os.path.join(model_path, 'mappings.pkl')
-            # Create directory for the model if it does not exist
             if not os.path.exists(self.model_path):
                 os.makedirs(self.model_path)
-            # Save the parameters to disk
             with open(self.parameters_path, 'wb') as f:
-                cPickle.dump(parameters, f)
+                pickle.dump(parameters, f)  # CHANGED: cPickle → pickle
         else:
             assert parameters is None and models_path is None
-            # Model location
             self.model_path = model_path
             self.parameters_path = os.path.join(model_path, 'parameters.pkl')
             self.mappings_path = os.path.join(model_path, 'mappings.pkl')
-            # Load the parameters and the mappings from disk
             with open(self.parameters_path, 'rb') as f:
-                self.parameters = cPickle.load(f)
+                self.parameters = pickle.load(f)  # CHANGED: cPickle → pickle
             self.reload_mappings()
         self.components = {}
 
     def save_mappings(self, id_to_word, id_to_char, id_to_tag):
-        """
-        We need to save the mappings if we want to use the model later.
-        """
         self.id_to_word = id_to_word
         self.id_to_char = id_to_char
         self.id_to_tag = id_to_tag
@@ -62,31 +52,22 @@ class Model(object):
                 'id_to_char': self.id_to_char,
                 'id_to_tag': self.id_to_tag,
             }
-            cPickle.dump(mappings, f)
+            pickle.dump(mappings, f)        # CHANGED: cPickle → pickle
 
     def reload_mappings(self):
-        """
-        Load mappings from disk.
-        """
         with open(self.mappings_path, 'rb') as f:
-            mappings = cPickle.load(f)
+            mappings = pickle.load(f)       # CHANGED: cPickle → pickle
         self.id_to_word = mappings['id_to_word']
         self.id_to_char = mappings['id_to_char']
         self.id_to_tag = mappings['id_to_tag']
 
     def add_component(self, param):
-        """
-        Add a new parameter to the network.
-        """
         if param.name in self.components:
             raise Exception('The network already has a parameter "%s"!'
                             % param.name)
         self.components[param.name] = param
 
     def save(self):
-        """
-        Write components values to disk.
-        """
         for name, param in self.components.items():
             param_path = os.path.join(self.model_path, "%s.mat" % name)
             if hasattr(param, 'params'):
@@ -96,9 +77,6 @@ class Model(object):
             scipy.io.savemat(param_path, param_values)
 
     def reload(self):
-        """
-        Load components values from disk.
-        """
         for name, param in self.components.items():
             param_path = os.path.join(self.model_path, "%s.mat" % name)
             param_values = scipy.io.loadmat(param_path)
@@ -108,34 +86,17 @@ class Model(object):
             else:
                 set_values(name, param, param_values[name])
 
-    def build(self,
-              dropout,
-              char_dim,
-              char_lstm_dim,
-              char_bidirect,
-              word_dim,
-              word_lstm_dim,
-              word_bidirect,
-              lr_method,
-              pre_emb,
-              crf,
-              cap_dim,
-              training=True,
-              **kwargs
-              ):
-        """
-        Build the network.
-        """
-        # Training parameters
+    def build(self, dropout, char_dim, char_lstm_dim, char_bidirect,
+              word_dim, word_lstm_dim, word_bidirect, lr_method,
+              pre_emb, crf, cap_dim, training=True, **kwargs):
+
         n_words = len(self.id_to_word)
         n_chars = len(self.id_to_char)
         n_tags = len(self.id_to_tag)
 
-        # Number of capitalization features
         if cap_dim:
             n_cap = 4
 
-        # Network variables
         is_train = T.iscalar('is_train')
         word_ids = T.ivector(name='word_ids')
         char_for_ids = T.imatrix(name='char_for_ids')
@@ -145,25 +106,20 @@ class Model(object):
         if cap_dim:
             cap_ids = T.ivector(name='cap_ids')
 
-        # Sentence length
         s_len = (word_ids if word_dim else char_pos_ids).shape[0]
 
-        # Final input (all word features)
         input_dim = 0
         inputs = []
 
-        #
         # Word inputs
-        #
         if word_dim:
             input_dim += word_dim
             word_layer = EmbeddingLayer(n_words, word_dim, name='word_layer')
             word_input = word_layer.link(word_ids)
             inputs.append(word_input)
-            # Initialize with pretrained embeddings
             if pre_emb and training:
                 new_weights = word_layer.embeddings.get_value()
-                print 'Loading pretrained embeddings from %s...' % pre_emb
+                print('Loading pretrained embeddings from %s...' % pre_emb)  # CHANGED: print()
                 pretrained = {}
                 emb_invalid = 0
                 for i, line in enumerate(codecs.open(pre_emb, 'r', 'utf-8')):
@@ -175,12 +131,11 @@ class Model(object):
                     else:
                         emb_invalid += 1
                 if emb_invalid > 0:
-                    print 'WARNING: %i invalid lines' % emb_invalid
+                    print('WARNING: %i invalid lines' % emb_invalid)  # CHANGED: print()
                 c_found = 0
                 c_lower = 0
                 c_zeros = 0
-                # Lookup table initialization
-                for i in xrange(n_words):
+                for i in range(n_words):                               # CHANGED: xrange → range
                     word = self.id_to_word[i]
                     if word in pretrained:
                         new_weights[i] = pretrained[word]
@@ -188,71 +143,60 @@ class Model(object):
                     elif word.lower() in pretrained:
                         new_weights[i] = pretrained[word.lower()]
                         c_lower += 1
-                    elif re.sub('\d', '0', word.lower()) in pretrained:
+                    elif re.sub(r'\d', '0', word.lower()) in pretrained:  # CHANGED: raw string
                         new_weights[i] = pretrained[
-                            re.sub('\d', '0', word.lower())
+                            re.sub(r'\d', '0', word.lower())           # CHANGED: raw string
                         ]
                         c_zeros += 1
                 word_layer.embeddings.set_value(new_weights)
-                print 'Loaded %i pretrained embeddings.' % len(pretrained)
-                print ('%i / %i (%.4f%%) words have been initialized with '
+                print('Loaded %i pretrained embeddings.' % len(pretrained))  # CHANGED: print()
+                print(('%i / %i (%.4f%%) words have been initialized with '
                        'pretrained embeddings.') % (
                             c_found + c_lower + c_zeros, n_words,
                             100. * (c_found + c_lower + c_zeros) / n_words
-                      )
-                print ('%i found directly, %i after lowercasing, '
+                      ))                                               # CHANGED: print()
+                print(('%i found directly, %i after lowercasing, '
                        '%i after lowercasing + zero.') % (
                           c_found, c_lower, c_zeros
-                      )
+                      ))                                               # CHANGED: print()
 
-        #
-        # Chars inputs
-        #
+        # Char inputs
         if char_dim:
             input_dim += char_lstm_dim
             char_layer = EmbeddingLayer(n_chars, char_dim, name='char_layer')
-
             char_lstm_for = LSTM(char_dim, char_lstm_dim, with_batch=True,
                                  name='char_lstm_for')
             char_lstm_rev = LSTM(char_dim, char_lstm_dim, with_batch=True,
                                  name='char_lstm_rev')
-
             char_lstm_for.link(char_layer.link(char_for_ids))
             char_lstm_rev.link(char_layer.link(char_rev_ids))
-
             char_for_output = char_lstm_for.h.dimshuffle((1, 0, 2))[
                 T.arange(s_len), char_pos_ids
             ]
             char_rev_output = char_lstm_rev.h.dimshuffle((1, 0, 2))[
                 T.arange(s_len), char_pos_ids
             ]
-
             inputs.append(char_for_output)
             if char_bidirect:
                 inputs.append(char_rev_output)
                 input_dim += char_lstm_dim
 
-        #
         # Capitalization feature
-        #
         if cap_dim:
             input_dim += cap_dim
             cap_layer = EmbeddingLayer(n_cap, cap_dim, name='cap_layer')
             inputs.append(cap_layer.link(cap_ids))
 
-        # Prepare final input
         inputs = T.concatenate(inputs, axis=1) if len(inputs) != 1 else inputs[0]
 
-        #
-        # Dropout on final input
-        #
+        # Dropout
         if dropout:
             dropout_layer = DropoutLayer(p=dropout)
             input_train = dropout_layer.link(inputs)
             input_test = (1 - dropout) * inputs
             inputs = T.switch(T.neq(is_train, 0), input_train, input_test)
 
-        # LSTM for words
+        # Word LSTM
         word_lstm_for = LSTM(input_dim, word_lstm_dim, with_batch=False,
                              name='word_lstm_for')
         word_lstm_rev = LSTM(input_dim, word_lstm_dim, with_batch=False,
@@ -263,43 +207,30 @@ class Model(object):
         word_rev_output = word_lstm_rev.h[::-1, :]
         if word_bidirect:
             final_output = T.concatenate(
-                [word_for_output, word_rev_output],
-                axis=1
-            )
+                [word_for_output, word_rev_output], axis=1)
             tanh_layer = HiddenLayer(2 * word_lstm_dim, word_lstm_dim,
                                      name='tanh_layer', activation='tanh')
             final_output = tanh_layer.link(final_output)
         else:
             final_output = word_for_output
 
-        # Sentence to Named Entity tags - Score
+        # Final scoring layer
         final_layer = HiddenLayer(word_lstm_dim, n_tags, name='final_layer',
                                   activation=(None if crf else 'softmax'))
         tags_scores = final_layer.link(final_output)
 
-        # No CRF
+        # CRF / No CRF cost
         if not crf:
             cost = T.nnet.categorical_crossentropy(tags_scores, tag_ids).mean()
-        # CRF
         else:
             transitions = shared((n_tags + 2, n_tags + 2), 'transitions')
-
             small = -1000
             b_s = np.array([[small] * n_tags + [0, small]]).astype(np.float32)
             e_s = np.array([[small] * n_tags + [small, 0]]).astype(np.float32)
             observations = T.concatenate(
-                [tags_scores, small * T.ones((s_len, 2))],
-                axis=1
-            )
-            observations = T.concatenate(
-                [b_s, observations, e_s],
-                axis=0
-            )
-
-            # Score from tags
+                [tags_scores, small * T.ones((s_len, 2))], axis=1)
+            observations = T.concatenate([b_s, observations, e_s], axis=0)
             real_path_score = tags_scores[T.arange(s_len), tag_ids].sum()
-
-            # Score from transitions
             b_id = theano.shared(value=np.array([n_tags], dtype=np.int32))
             e_id = theano.shared(value=np.array([n_tags + 1], dtype=np.int32))
             padded_tags_ids = T.concatenate([b_id, tag_ids, e_id], axis=0)
@@ -307,7 +238,6 @@ class Model(object):
                 padded_tags_ids[T.arange(s_len + 1)],
                 padded_tags_ids[T.arange(s_len + 1) + 1]
             ].sum()
-
             all_paths_scores = forward(observations, transitions)
             cost = - (real_path_score - all_paths_scores)
 
@@ -341,7 +271,7 @@ class Model(object):
             self.add_component(tanh_layer)
             params.extend(tanh_layer.params)
 
-        # Prepare train and eval inputs
+        # Eval and train inputs
         eval_inputs = []
         if word_dim:
             eval_inputs.append(word_ids)
@@ -354,7 +284,7 @@ class Model(object):
             eval_inputs.append(cap_ids)
         train_inputs = eval_inputs + [tag_ids]
 
-        # Parse optimization method parameters
+        # Parse optimization method
         if "-" in lr_method:
             lr_method_name = lr_method[:lr_method.find('-')]
             lr_method_parameters = {}
@@ -366,10 +296,11 @@ class Model(object):
             lr_method_name = lr_method
             lr_method_parameters = {}
 
-        # Compile training function
-        print 'Compiling...'
+        # Compile
+        print('Compiling...')                                          # CHANGED: print()
         if training:
-            updates = Optimization(clip=5.0).get_updates(lr_method_name, cost, params, **lr_method_parameters)
+            updates = Optimization(clip=5.0).get_updates(
+                lr_method_name, cost, params, **lr_method_parameters)
             f_train = theano.function(
                 inputs=train_inputs,
                 outputs=cost,
@@ -379,7 +310,6 @@ class Model(object):
         else:
             f_train = None
 
-        # Compile evaluation function
         if not crf:
             f_eval = theano.function(
                 inputs=eval_inputs,
