b201e3265234bd0da7b2c16010596bdbb0314032
Satvik-Somashekar <satviksomshekar2004@gmail.com>
2026-04-13 13:18:16 +0530

    Fix raise Exception in optimization.py - Python 3 port

--- optimization.py
+++ optimization.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# Modified by Satvik Somashekar - Python 3 port
+
 import numpy as np
 import theano
 import theano.tensor as T
@@ -9,15 +12,9 @@ device = theano.config.device
 class Optimization:
 
     def __init__(self, clip=None):
-        """
-        Initialization
-        """
         self.clip = clip
 
     def get_gradients(self, cost, params):
-        """
-        Compute the gradients, and clip them if required.
-        """
         if self.clip is None:
             return T.grad(cost, params)
         else:
@@ -28,9 +25,6 @@ class Optimization:
             )
 
     def get_updates(self, method, cost, params, *args, **kwargs):
-        """
-        Compute the updates for different optimizers.
-        """
         if method == 'sgd':
             updates = self.sgd(cost, params, **kwargs)
         elif method == 'sgdmomentum':
@@ -44,36 +38,23 @@ class Optimization:
         elif method == 'rmsprop':
             updates = self.rmsprop(cost, params, **kwargs)
         else:
-            raise("Not implemented learning method: %s" % method)
+            raise Exception("Not implemented learning method: %s" % method)  # CHANGED
         return updates
 
     def sgd(self, cost, params, lr=0.01):
-        """
-        Stochatic gradient descent.
-        """
         lr = theano.shared(np.float32(lr).astype(floatX))
-
         gradients = self.get_gradients(cost, params)
-
         updates = []
         for p, g in zip(params, gradients):
             updates.append((p, p - lr * g))
-
         return updates
 
     def sgdmomentum(self, cost, params, lr=0.01, momentum=0.9):
-        """
-        Stochatic gradient descent with momentum. Momentum has to be in [0, 1)
-        """
-        # Check that the momentum is a correct value
         assert 0 <= momentum < 1
-
         lr = theano.shared(np.float32(lr).astype(floatX))
         momentum = theano.shared(np.float32(momentum).astype(floatX))
-
         gradients = self.get_gradients(cost, params)
         velocities = [theano.shared(np.zeros_like(param.get_value(borrow=True)).astype(floatX)) for param in params]
-
         updates = []
         for param, gradient, velocity in zip(params, gradients, velocities):
             new_velocity = momentum * velocity - lr * gradient
@@ -82,15 +63,10 @@ class Optimization:
         return updates
 
     def adagrad(self, cost, params, lr=1.0, epsilon=1e-6):
-        """
-        Adagrad. Based on http://www.ark.cs.cmu.edu/cdyer/adagrad.pdf
-        """
         lr = theano.shared(np.float32(lr).astype(floatX))
         epsilon = theano.shared(np.float32(epsilon).astype(floatX))
-
         gradients = self.get_gradients(cost, params)
         gsums = [theano.shared(np.zeros_like(param.get_value(borrow=True)).astype(floatX)) for param in params]
-
         updates = []
         for param, gradient, gsum in zip(params, gradients, gsums):
             new_gsum = gsum + gradient ** 2.
@@ -99,17 +75,11 @@ class Optimization:
         return updates
 
     def adadelta(self, cost, params, rho=0.95, epsilon=1e-6):
-        """
-        Adadelta. Based on:
-        http://www.matthewzeiler.com/pubs/googleTR2012/googleTR2012.pdf
-        """
         rho = theano.shared(np.float32(rho).astype(floatX))
         epsilon = theano.shared(np.float32(epsilon).astype(floatX))
-
         gradients = self.get_gradients(cost, params)
         accu_gradients = [theano.shared(np.zeros_like(param.get_value(borrow=True)).astype(floatX)) for param in params]
         accu_deltas = [theano.shared(np.zeros_like(param.get_value(borrow=True)).astype(floatX)) for param in params]
-
         updates = []
         for param, gradient, accu_gradient, accu_delta in zip(params, gradients, accu_gradients, accu_deltas):
             new_accu_gradient = rho * accu_gradient + (1. - rho) * gradient ** 2.
@@ -121,48 +91,32 @@ class Optimization:
         return updates
 
     def adam(self, cost, params, lr=0.001, beta1=0.9, beta2=0.999, epsilon=1e-8):
-        """
-        Adam. Based on http://arxiv.org/pdf/1412.6980v4.pdf
-        """
         updates = []
         gradients = self.get_gradients(cost, params)
-
         t = theano.shared(np.float32(1.).astype(floatX))
-
         for param, gradient in zip(params, gradients):
             value = param.get_value(borrow=True)
             m_prev = theano.shared(np.zeros(value.shape, dtype=value.dtype), broadcastable=param.broadcastable)
             v_prev = theano.shared(np.zeros(value.shape, dtype=value.dtype), broadcastable=param.broadcastable)
-
             m = beta1 * m_prev + (1. - beta1) * gradient
             v = beta2 * v_prev + (1. - beta2) * gradient ** 2.
             m_hat = m / (1. - beta1 ** t)
             v_hat = v / (1. - beta2 ** t)
             theta = param - (lr * m_hat) / (T.sqrt(v_hat) + epsilon)
-
             updates.append((m_prev, m))
             updates.append((v_prev, v))
             updates.append((param, theta))
-
         updates.append((t, t + 1.))
         return updates
 
     def rmsprop(self, cost, params, lr=0.001, rho=0.9, eps=1e-6):
-        """
-        RMSProp.
-        """
         lr = theano.shared(np.float32(lr).astype(floatX))
-
         gradients = self.get_gradients(cost, params)
         accumulators = [theano.shared(np.zeros_like(p.get_value()).astype(floatX)) for p in params]
-
         updates = []
-
         for param, gradient, accumulator in zip(params, gradients, accumulators):
             new_accumulator = rho * accumulator + (1 - rho) * gradient ** 2
             updates.append((accumulator, new_accumulator))
-
             new_param = param - lr * gradient / T.sqrt(new_accumulator + eps)
             updates.append((param, new_param))
-
         return updates
